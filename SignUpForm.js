da3c87ee5c966e52385e608e677f2a50de864b47
Mohammed Musthaqil A <115704981+Musthaqil@users.noreply.github.com>
2023-11-02 21:34:57 +0530

    Add files via upload

---
+++ SignUpForm.js
@@ -0,0 +1,69 @@
+// src/components/SignUpForm.js
+import React, { useState } from 'react';
+
+function SignUpForm() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send form data to your backend for processing and MongoDB storage
+    // You will implement this part in the backend
+
+    // Clear the form after submission
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpForm;
